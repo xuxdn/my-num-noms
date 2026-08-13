ea00580ab144235c3b4f783dfab0581f0b1b7741
xuxdn <xuxdn@kookmin.ac.kr>
2026-08-13 18:59:59 +0900

    Add January Num Noms characters

--- characters.js
+++ characters.js
@@ -1,92 +1,282 @@
 const characters = {
 
   "1-1": {
-    name: "Strawberry",
+    name: "Nilla Cream",
     series: "Series 1",
-    number: "114",
-    emoji: "🍓",
+    number: "101",
+    emoji: "🍦",
     image: "",
-    description: "상큼하고 달콤한 매력을 가진 Num Noms예요! ♡"
+    description: "부드럽고 달콤한 매력의 Num Noms예요! ♡"
   },
 
   "1-2": {
-    name: "Lemonade",
+    name: "Nilla Swirl",
     series: "Series 1",
-    number: "106",
-    emoji: "🍋",
-    description: "톡톡 튀는 상큼함이 매력적인 Num Noms예요! ✨"
+    number: "102",
+    emoji: "🍦",
+    image: "",
+    description: "달콤하고 부드러운 매력이 가득해요! ✨"
   },
 
   "1-3": {
-    name: "Orange",
+    name: "Choco Nana",
     series: "Series 1",
-    number: "107",
-    emoji: "🍊",
-    description: "밝고 상큼한 에너지를 가진 Num Noms예요! ♡"
+    number: "103",
+    emoji: "🍫",
+    image: "",
+    description: "초콜릿과 바나나의 달콤한 매력이 있어요! 🍫"
   },
 
   "1-4": {
     name: "Nana Cream",
     series: "Series 1",
-    number: "108",
+    number: "104",
     emoji: "🍌",
-    description: "부드럽고 달콤한 매력을 가진 Num Noms예요! 🎀"
+    image: "",
+    description: "부드럽고 달콤한 바나나 크림 느낌이에요! 🎀"
   },
 
   "1-5": {
-    name: "Bubblegum",
+    name: "Lemony Burst",
     series: "Series 1",
-    number: "112",
-    emoji: "🩷",
-    description: "통통 튀는 달콤함이 매력인 Num Noms예요! ✨"
+    number: "105",
+    emoji: "🍋",
+    image: "",
+    description: "상큼하고 톡톡 튀는 레몬 같은 매력이 있어요! ✨"
   },
 
   "1-6": {
-    name: "Sweetie",
+    name: "Pinkie Lemonade",
     series: "Series 1",
-    number: "113",
-    emoji: "🍬",
-    description: "달콤하고 사랑스러운 분위기의 Num Noms예요! 💗"
+    number: "106",
+    emoji: "🍋",
+    image: "",
+    description: "달콤상큼한 레모네이드 같은 매력이 있어요! 💕"
   },
 
   "1-7": {
-    name: "Mango",
+    name: "Orange Swirl",
     series: "Series 1",
-    number: "116",
-    emoji: "🥭",
-    description: "달콤하고 상큼한 열대과일 느낌의 Num Noms예요! 🌴"
+    number: "107",
+    emoji: "🍊",
+    image: "",
+    description: "상큼한 오렌지처럼 밝은 매력이 있어요! 🍊"
   },
 
   "1-8": {
-    name: "Peach",
+    name: "Patty Peach",
     series: "Series 1",
-    number: "117",
+    number: "108",
     emoji: "🍑",
-    description: "말랑말랑하고 달콤한 매력을 가진 Num Noms예요! 🎀"
+    image: "",
+    description: "복숭아처럼 달콤하고 사랑스러워요! 🎀"
   },
 
   "1-9": {
-    name: "Blueberry",
+    name: "Mimi Mango",
     series: "Series 1",
-    number: "121",
-    emoji: "🫐",
-    description: "귀엽고 상큼한 블루베리 느낌의 Num Noms예요! 💙"
+    number: "109",
+    emoji: "🥭",
+    image: "",
+    description: "달콤하고 상큼한 망고 같은 매력이 있어요! 🥭"
   },
 
   "1-10": {
-    name: "Raspberry",
+    name: "Sweetie Strawberry",
     series: "Series 1",
-    number: "122",
+    number: "110",
     emoji: "🍓",
-    description: "새콤달콤하고 발랄한 매력의 Num Noms예요! 💕"
+    image: "",
+    description: "달콤하고 사랑스러운 딸기 매력이 가득해요! 🍓"
   },
 
   "1-11": {
-    name: "Vanilla",
+    name: "Betsy Bubblegum",
+    series: "Series 1",
+    number: "111",
+    emoji: "🩷",
+    image: "",
+    description: "통통 튀는 버블검 같은 매력이 있어요! 🩷"
+  },
+
+  "1-12": {
+    name: "Cheery Cherie",
+    series: "Series 1",
+    number: "112",
+    emoji: "🍒",
+    image: "",
+    description: "체리처럼 발랄하고 상큼한 매력이 있어요! 🍒"
+  },
+
+  "1-13": {
+    name: "Berry Berry Swirl",
+    series: "Series 1",
+    number: "113",
+    emoji: "🫐",
+    image: "",
+    description: "달콤한 베리 스월 같은 매력이 가득해요! 💕"
+  },
+
+  "1-14": {
+    name: "Raspberry Cream",
+    series: "Series 1",
+    number: "114",
+    emoji: "🍓",
+    image: "",
+    description: "새콤달콤한 라즈베리 크림 같은 매력이 있어요! 🍓"
+  },
+
+  "1-15": {
+    name: "Mary Mulberry",
+    series: "Series 1",
+    number: "115",
+    emoji: "🫐",
+    image: "",
+    description: "달콤한 베리 느낌의 사랑스러운 매력이 있어요! 💜"
+  },
+
+  "1-16": {
+    name: "Wendy Wild Berry",
+    series: "Series 1",
+    number: "116",
+    emoji: "🫐",
+    image: "",
+    description: "상큼한 와일드베리처럼 톡톡 튀는 매력이 있어요! 💜"
+  },
+
+  "1-17": {
+    name: "Bonnie Blueberry",
+    series: "Series 1",
+    number: "117",
+    emoji: "🫐",
+    image: "",
+    description: "귀엽고 상큼한 블루베리 매력이 가득해요! 💙"
+  },
+
+  "1-18": {
+    name: "Cherry Choco",
+    series: "Series 1",
+    number: "118",
+    emoji: "🍒",
+    image: "",
+    description: "체리와 초콜릿의 달콤한 느낌이 매력적이에요! 🍫"
+  },
+
+  "1-19": {
+    name: "Minty Swirl",
+    series: "Series 1",
+    number: "119",
+    emoji: "🌿",
+    image: "",
+    description: "상쾌하고 시원한 민트 느낌이에요! 💚"
+  },
+
+  "1-20": {
+    name: "Candie Puffs",
+    series: "Series 1",
+    number: "120",
+    emoji: "🍬",
+    image: "",
+    description: "알록달록 달콤한 캔디 같은 매력이 있어요! 🍬"
+  },
+
+  "1-21": {
+    name: "Red Velvety",
+    series: "Series 1",
+    number: "121",
+    emoji: "❤️",
+    image: "",
+    description: "부드럽고 달콤한 레드벨벳 같은 매력이 있어요! ❤️"
+  },
+
+  "1-22": {
+    name: "Betty B-Day",
+    series: "Series 1",
+    number: "122",
+    emoji: "🎂",
+    image: "",
+    description: "생일처럼 특별하고 달콤한 매력이 있어요! 🎂"
+  },
+
+  "1-23": {
+    name: "Choco Berry",
     series: "Series 1",
     number: "123",
+    emoji: "🍫",
+    image: "",
+    description: "초콜릿과 베리의 달콤한 조합 같은 매력이 있어요! 💕"
+  },
+
+  "1-24": {
+    name: "Lulu Licorice",
+    series: "Series 1",
+    number: "124",
+    emoji: "🖤",
+    image: "",
+    description: "독특하고 달콤한 리코리스 같은 매력이 있어요! 🖤"
+  },
+
+  "1-25": {
+    name: "Nilla Twirl",
+    series: "Series 1",
+    number: "125",
     emoji: "🍦",
-    description: "부드럽고 달콤한 매력의 Num Noms예요! 🍦"
+    image: "",
+    description: "부드러운 바닐라처럼 포근한 매력이 있어요! ♡"
+  },
+
+  "1-26": {
+    name: "Caramel Cream",
+    series: "Series 1",
+    number: "126",
+    emoji: "🍮",
+    image: "",
+    description: "진하고 달콤한 카라멜 크림 같은 매력이 있어요! 🍮"
+  },
+
+  "1-27": {
+    name: "Nana Berry",
+    series: "Series 1",
+    number: "127",
+    emoji: "🍌",
+    image: "",
+    description: "바나나와 베리처럼 달콤한 매력이 있어요! 💕"
+  },
+
+  "1-28": {
+    name: "Billy Banana",
+    series: "Series 1",
+    number: "128",
+    emoji: "🍌",
+    image: "",
+    description: "귀엽고 달콤한 바나나 같은 매력이 있어요! 🍌"
+  },
+
+  "1-29": {
+    name: "Tango Mango",
+    series: "Series 1",
+    number: "129",
+    emoji: "🥭",
+    image: "",
+    description: "상큼하고 통통 튀는 망고 같은 매력이 있어요! 🥭"
+  },
+
+  "1-30": {
+    name: "Peachy Cream",
+    series: "Series 1",
+    number: "130",
+    emoji: "🍑",
+    image: "",
+    description: "복숭아처럼 말랑하고 달콤한 매력이 있어요! 🍑"
+  },
+
+  "1-31": {
+    name: "Lisa Lemon",
+    series: "Series 1",
+    number: "131",
+    emoji: "🍋",
+    image: "",
+    description: "상큼하고 귀여운 레몬 같은 매력이 있어요! 🍋"
   }
 
 };
